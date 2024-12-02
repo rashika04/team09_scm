74d39c4240d3599c6462423c02877c7588ac2346
Rashika Gupta <rashika.22scse1011958@galgotiasuniversity.edu.in>
2024-12-02 12:22:42 +0530

    Initial Commit

---
+++ team09_scm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  team09_scm
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
